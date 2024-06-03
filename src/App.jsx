96e00fbdafdbfba86e68c36c32e57de15ac12b2a
AlenaDenysenko <naopiya28@gmai.com>
2024-06-03 19:56:32 +0300

    HW2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    useEffect(() => {
+        const savedFeedback = localStorage.getItem('feedback');
+        if (savedFeedback) {
+            setFeedback(JSON.parse(savedFeedback));
+        }
+    }, []);
 
-export default App
+    useEffect(() => {
+        localStorage.setItem('feedback', JSON.stringify(feedback));
+    }, [feedback]);
+
+    const updateFeedback = (feedbackType) => {
+        setFeedback(prevFeedback => ({
+            ...prevFeedback,
+            [feedbackType]: prevFeedback[feedbackType] + 1,
+        }));
+    };
+
+    const resetFeedback = () => {
+        setFeedback({ good: 0, neutral: 0, bad: 0 });
+    };
+
+    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+    const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100) || 0;
+
+    return (
+        <div>
+            <h1>Sip Happens Café</h1>
+            <p>Please leave your feedback about our service by selecting one of the options below.</p>
+            <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
+            {totalFeedback > 0 ? (
+                <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} />
+            ) : (
+                <Notification message="No feedback given yet" />
+            )}
+        </div>
+    );
+};
+
+export default App;
